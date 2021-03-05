003e181cd3a13ad665a9ae30ea17767b9adb2ebb
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-03-05 19:47:58 +0200

    Refactor IO handlign from ActiveStorage to its own module

--- lib/ratonvirus/storage/active_storage.rb
+++ lib/ratonvirus/storage/active_storage.rb
@@ -3,6 +3,8 @@
 module Ratonvirus
   module Storage
     class ActiveStorage < Base
+      include Ratonvirus::Storage::Support::IoHandling
+
       def changed?(record, attribute)
         resource = record.public_send attribute
         !resource.record.attachment_changes[resource.name].nil?
@@ -100,39 +102,6 @@ module Ratonvirus
         attachable = change.attachment.blob if attachable.is_a?(String)
 
         yield processable([change.attachment, attachable])
-      end
-
-      # This creates a local copy of the io contents for the scanning process. A
-      # local copy is needed for processing because the io object may be a file
-      # stream in the memory which may not have a path associated with it on the
-      # filesystem.
-      def io_path(io, extension)
-        tempfile = Tempfile.open(
-          ["Ratonvirus", extension],
-          tempdir
-        )
-        # Important for the scanner to be able to access the file.
-        prepare_for_scanner tempfile.path
-
-        begin
-          tempfile.binmode
-          IO.copy_stream(io, tempfile)
-          tempfile.flush
-          tempfile.rewind
-
-          yield tempfile.path
-        ensure
-          tempfile.close!
-        end
-      end
-
-      def tempdir
-        Dir.tmpdir
-      end
-
-      def prepare_for_scanner(filepath)
-        # Important for the scanner to be able to access the file.
-        File.chmod(0o644, filepath)
       end
     end
   end
