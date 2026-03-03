d5f2bba49ace66f2855d12268b7fec43662f95d8
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2026-03-03 15:28:13 +0200

    Fix checksum mismatch for StringIO
    Do not change the original StringIO position when converting it
    to a temp file.

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -10,6 +10,19 @@ module Ratonvirus
       def ratonvirus_file_fixture(filepath)
         "#{spec_path}/fixtures/files/#{filepath}"
       end
+
+      def activestorage_digest(io)
+        # This is how ActiveStorage would calculate the digest.
+        #
+        # See:
+        # https://github.com/rails/rails/blob/afd103d69abb7441da3d2ac5c737f8de3e678779/activestorage/lib/active_storage/service.rb#L161-L168
+        OpenSSL::Digest::MD5.new.tap do |checksum|
+          read_buffer = "".b
+          io.read(nil, read_buffer)
+          checksum << read_buffer
+          io.rewind
+        end.base64digest
+      end
     end
   end
 end
